13b2f7c93f479c309617c6190643fe4059258be4
LD211 <93799505+LD211@users.noreply.github.com>
2024-03-05 14:43:38 -0500

    added scripting

---
+++ index.js
@@ -0,0 +1,14 @@
+const sort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
+console.log(sort([5, 3, 5, 6, 7, 8]));
