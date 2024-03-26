ec5b7a8717b82cb5d77d2e80aa730886c17b6b6b
LD211 <93799505+LD211@users.noreply.github.com>
2024-03-26 14:13:07 -0400

    cleaned up subcourse styles

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const gid = (name) => {
-  return document.QuerySelector(name);
+  return document.querySelector("." + name);
 };
 const d = gid("todaysdate");
+const courses = gid("courses");
 const getDate = () => {
   const date = new Date();
+  const minute = date.getMinutes();
+  const hour = date.getHours();
+  const day = date.getDay();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+  console.log(d);
+  d.innerText = new Date().toString(); //year + " " + month + " " + day;
 };
+getDate();
+
+const openLink = (site) => {
+  location.href = "./" + site + ".html";
+};
+
+const spawnCourse = (name, photourl) => {
+  const course = gid("subcourse");
+  const cloneCourse = document.cloneElement(course);
+};
+
+gid("subcourse").addEventListener("click", () => {
+  location.href = "Course_Page.html";
+});
